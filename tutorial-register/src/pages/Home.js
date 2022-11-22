d6bd13e094109f631f228043842563049485936b
mai-rad98 <mradipodi@gmail.com>
2022-11-22 11:07:32 +0200

    pricing table

--- tutorial-register/src/pages/Home.js
+++ tutorial-register/src/pages/Home.js
@@ -8,23 +8,7 @@ function Home() {
     <Typography>Please fill out the from below to enroll</ Typography>
     <></>
     <Form />
-   {/*  <Box
-    sx={{
-      justifyContent:'center', 
-      display: 'flex',
-      flexWrap: 'wrap',
-      '& > :not(style)': {
-        m: 1,
-        width: 1000,
-        height: 1000,
-      },
-    }}
-  >
-    <Paper elevation={5}> <Form /></Paper>
-    
-
-    
-  </Box> */}
+   
   </>
 );
   
--- tutorial-register/src/pages/Pricing.js
+++ tutorial-register/src/pages/Pricing.js
@@ -1,41 +1,57 @@
-import React from 'react'
+import React from 'react';
+import {PricingTable, PricingSlot, PricingDetail} from 'react-pricing-table'
+import { Grid,Typography ,Paper} from '@mui/material';
+ 
+function Pricing (){
 
-import { Box ,Grid} from '@mui/material'
-import Typography from '@mui/material/Typography'
-import Paper from '@mui/material/Paper'
-
-
-
-
-
-function Pricing() {
-  return (
-    <Grid >
-      <Typography justifyContent={"center"}>Pricing</Typography>
-      <Typography>
-Quickly build a effective pricing table for your potential customers with this layout. It's built with default MUI components with little customization.</Typography>
-
-        <Box  sx={{
-        justifyContent:'center',
-        display: 'flex',
-        flexWrap: 'wrap',
-        '& > :not(style)': {
-          m: 1,
-          width: 350,
-          height: 450,
-        },
-      }}
-      >
-        <Paper elevation={3}>
-          <Typography justifyContent={"center"}>PRICING</Typography>
+  const paperStyle={padding :20,height:'70vh',width:1000, margin:"20px auto"} 
+  
+    return  (
+      <Grid>
+        <Grid>
+          <Typography
+          justifyContent={"center"}
+          variant="h3">Pricing
+          </Typography>
+          <Typography variant="h6">
+Build an effective pricing table for your potential customers with this layout. It's built with default MUI components with little customization and reat-pricing-table</Typography>
+        </Grid>
+        <Grid><Paper elevation={14} style={paperStyle}>
           
+          
+        <PricingTable  highlightColor='#1976D2'>
+     <PricingSlot   buttonText='TRY IT FREE' title='FREE' priceText='$0/month'>
+        <PricingDetail> <b>15</b> projects</PricingDetail>
+        <PricingDetail> <b>5 GB</b> storage</PricingDetail>
+        <PricingDetail> <b>5</b> users</PricingDetail>
+        <PricingDetail strikethrough> <b>Time tracking</b></PricingDetail>
+    </PricingSlot>
+    <PricingSlot highlighted buttonText='SIGN UP' title='BASIC' priceText='$24/month'>
+        <PricingDetail> <b>35</b> projects</PricingDetail>
+        <PricingDetail> <b>15 GB</b> storage</PricingDetail>
+        <PricingDetail> <b>Unlimited</b> users</PricingDetail>
+        <PricingDetail> <b>Time tracking</b></PricingDetail>
+    </PricingSlot>
+    <PricingSlot   buttonText='SIGN UP' title='PROFESSIONAL' priceText='$99/month'>
+        <PricingDetail> <b>100</b> projects</PricingDetail>
+        <PricingDetail> <b>30 GB</b> storage</PricingDetail>
+        <PricingDetail> <b>Unlimited</b> users</PricingDetail>
+        <PricingDetail> <b>Time tracking</b></PricingDetail>
+    </PricingSlot>
+    <PricingSlot   buttonText='SIGN UP' title='ENTERPRISE' priceText='$200/month'>
+        <PricingDetail> <b>Unlimited</b> projects</PricingDetail>
+        <PricingDetail> <b>75 GB</b> storage</PricingDetail>
+        <PricingDetail> <b>Unlimited</b> users</PricingDetail>
+        <PricingDetail> <b>Time tracking</b></PricingDetail>
+    </PricingSlot>
+</PricingTable>
           
           </Paper>
-        <Paper elevation={3}><Typography justifyContent={"center"}>PRICING</Typography></Paper>
-        <Paper elevation={3}><Typography justifyContent={"center"}>PRICING</Typography></Paper>
-      </Box>
-    </Grid>
-  )
-}
+          </Grid>
+     
 
-export default Pricing
+</Grid>
+    )
+  }
+
+  export default Pricing
